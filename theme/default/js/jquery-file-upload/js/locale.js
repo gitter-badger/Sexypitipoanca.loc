f4b8e346316d8462f0942a7f49ad520382bd9bb9
tsergium <tsergium@gmail.com>
2014-11-22 16:51:42 +0200

    Theme Directory

---
+++ theme/default/js/jquery-file-upload/js/locale.js
@@ -0,0 +1,27 @@
+/*
+ * jQuery File Upload Plugin Localization Example 6.5
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2012, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+window.locale = {
+    "fileupload": {
+        "errors": {
+            "maxFileSize": "Fisier prea mare",
+            "minFileSize": "Fisier prea mic",
+            "acceptFileTypes": "Extensia nu e permisa",
+            "maxNumberOfFiles": "Numar maxim de fisiere depasit",
+            "uploadedBytes": "Dimensiune maxima upload depasita",
+            "emptyResult": "Eroare upload fisier"
+        },
+        "error": "Eroare",
+        "start": "Upload",
+        "cancel": "Anuleaza",
+        "destroy": "Sterge"
+    }
+};
