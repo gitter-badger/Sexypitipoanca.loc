cdeea25a8c97426499b907947ac93e360e16d86e
Sergiu Tomsa <sergiu.tomsa@shopwings.com>
2015-09-23 14:13:10 +0200

    Added a base controller

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -1,21 +1,6 @@
 <?php
-class ApiController extends Zend_Controller_Action
+class ApiController extends TS_Controller_Action
 {
-    public function init()
-    {
-        $this->_helper->viewRenderer->setNoRender();
-        $this->_helper->getHelper('layout')->disableLayout();
-        $bootstrap = $this->getInvokeArg('bootstrap');
-        if ($bootstrap->hasResource('db')) {
-            $this->db = $bootstrap->getResource('db');
-        }
-
-        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
-        $this->view->message = $this->_flashMessenger->getMessages();
-
-        $this->getResponse()->setHeader('Content-Type', 'application/json');
-    }
-
     public function indexAction()
     {
         $body = $this->getRequest()->getRawBody();
---
+++ library/TS/Controller/Action.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergiu
+ * Date: 9/23/15
+ * Time: 2:04 PM
+ */
+class TS_Controller_Action extends Zend_Controller_Action
+{
+    public function init()
+    {
+        $this->_helper->viewRenderer->setNoRender();
+        $this->_helper->getHelper('layout')->disableLayout();
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        if ($bootstrap->hasResource('db')) {
+            $this->db = $bootstrap->getResource('db');
+        }
+
+        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+        $this->view->message = $this->_flashMessenger->getMessages();
+
+        $this->getResponse()->setHeader('Content-Type', 'application/json');
+    }
+}
